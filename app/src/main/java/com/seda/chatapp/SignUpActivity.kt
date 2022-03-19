9bac7cb95a801ee8105a5577617c874d64fe7ecf
SedaNur35 <sednrondr@gmail.com>
2022-03-20 00:25:32 +0300

    commit

---
+++ app/src/main/java/com/seda/chatapp/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.seda.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
